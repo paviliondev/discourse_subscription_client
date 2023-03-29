a6fa6097c796e4de0011a14cd24734dde3eda8bf
Angus McLeod <angus@mcleod.org.au>
2023-03-29 17:04:58 +0800

    Destroy subscription client authorisation even if key revocation fails

--- app/models/subscription_client_supplier.rb
+++ app/models/subscription_client_supplier.rb
@@ -10,13 +10,9 @@ class SubscriptionClientSupplier < ActiveRecord::Base
   scope :authorized, -> { where("api_key IS NOT NULL") }
 
   def destroy_authorization
-    if DiscourseSubscriptionClient::Authorization.revoke(self)
-      update(api_key: nil, user_id: nil, authorized_at: nil)
-      deactivate_all_subscriptions!
-      true
-    else
-      false
-    end
+    DiscourseSubscriptionClient::Authorization.revoke(self)
+    update(api_key: nil, user_id: nil, authorized_at: nil)
+    deactivate_all_subscriptions!
   end
 
   def authorized?
--- spec/models/subscription_client_supplier_spec.rb
+++ spec/models/subscription_client_supplier_spec.rb
@@ -9,20 +9,38 @@ describe SubscriptionClientSupplier do
   let!(:resource) { Fabricate(:subscription_client_resource, supplier: supplier) }
   let!(:subscription) { Fabricate(:subscription_client_subscription, resource: resource, subscribed: true) }
 
-  it "destroys authorization" do
-    stub_request(:post, "#{supplier.url}/user-api-key/revoke").to_return(status: 200, body: '{ "success": "OK" }')
+  describe "#destroy_authorization" do
+    context "when api key revocation succeeds" do
+      it "destroys authorization" do
+        stub_request(:post, "#{supplier.url}/user-api-key/revoke").to_return(status: 200, body: "{ \"success\": \"OK\" }")
 
-    expect(supplier.destroy_authorization).to eq(true)
-    expect(supplier.api_key).to eq(nil)
-    expect(supplier.user_id).to eq(nil)
-    expect(supplier.authorized_at).to eq(nil)
-    expect(subscription.reload.subscribed).to eq(false)
+        expect(supplier.destroy_authorization).to eq(1)
+        expect(supplier.api_key).to eq(nil)
+        expect(supplier.user_id).to eq(nil)
+        expect(supplier.authorized_at).to eq(nil)
+        expect(subscription.reload.subscribed).to eq(false)
+      end
+    end
+
+    context "when api key revocation fails" do
+      it "destroys authorization" do
+        stub_request(:post, "#{supplier.url}/user-api-key/revoke").to_return(status: 400, body: "{ \"failed\": \"FAILED\" }")
+
+        expect(supplier.destroy_authorization).to eq(1)
+        expect(supplier.api_key).to eq(nil)
+        expect(supplier.user_id).to eq(nil)
+        expect(supplier.authorized_at).to eq(nil)
+        expect(subscription.reload.subscribed).to eq(false)
+      end
+    end
   end
 
-  it "maps product slugs" do
-    expect(supplier.product_slugs).to eq({})
-    supplier.products = [{ product_id: "prod_CBTNpi3fqWWkq0", product_slug: "business" }].as_json
-    supplier.save!
-    expect(supplier.reload.product_slugs).to eq({ "prod_CBTNpi3fqWWkq0" => "business" })
+  describe "#product_slugs" do
+    it "maps product slugs" do
+      expect(supplier.product_slugs).to eq({})
+      supplier.products = [{ product_id: "prod_CBTNpi3fqWWkq0", product_slug: "business" }].as_json
+      supplier.save!
+      expect(supplier.reload.product_slugs).to eq({ "prod_CBTNpi3fqWWkq0" => "business" })
+    end
   end
 end
