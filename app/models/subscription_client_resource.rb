3f198c8e770349e0d2066dab2963e79834a44de7
Angus McLeod <angus@mcleod.org.au>
2024-09-17 19:17:25 +0200

    Update subscription_client_resource.rb

--- app/models/subscription_client_resource.rb
+++ app/models/subscription_client_resource.rb
@@ -4,15 +4,21 @@ class SubscriptionClientResource < ActiveRecord::Base
   belongs_to :supplier, class_name: "SubscriptionClientSupplier"
   has_many :notices, class_name: "SubscriptionClientNotice", as: :notice_subject, dependent: :destroy
   has_many :subscriptions, foreign_key: "resource_id", class_name: "SubscriptionClientSubscription", dependent: :destroy
+
+  def source_url(bucket)
+    "s3://#{access_key_id}:#{secret_access_key}@#{bucket}"
+  end
 end
 
 # == Schema Information
 #
 # Table name: subscription_client_resources
 #
-#  id          :bigint           not null, primary key
-#  supplier_id :bigint
-#  name        :string           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                :bigint           not null, primary key
+#  supplier_id       :bigint
+#  name              :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  access_key_id     :string
+#  secret_access_key :string
 #
